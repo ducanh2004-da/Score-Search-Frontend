dc1bb7a2c593ff1d94a4ae26a26300081677a01a
Đỗ Đức Anh <anhdott1@gmail.com>
2026-04-24 07:39:19 +0700

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { QueryClientProvider } from '@tanstack/react-query'
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 import { queryClient } from './configs/query-client.config';
 import reactArrayToTree from 'react-array-to-tree'
 import App from './App.tsx'
